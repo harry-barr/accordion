9db5bd1a6de4beecad3841b690fb604fd564cc88
Harry Barr <harrybarr530@gmail.com>
2024-11-04 14:24:26 +0000

    completed accordion

---
+++ accordion/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Accordion from "./Components/accordion";
+
+function App() {
+  return (
+    <div className="App">
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
